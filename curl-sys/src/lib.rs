f266f512bf34ac094065616e719c6622011bfa41
jiacai2050 <dev@liujiacai.net>
2024-11-09 18:11:31 +0800

    add err handle

--- curl-sys/src/lib.rs
+++ curl-sys/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
 use std::os::raw::{c_char, c_int, c_void};
 use std::slice;
 
@@ -7,6 +7,8 @@ extern "C" {
     fn curl_easy_setopt(handle: *mut CURL, option: CURLoption, ...) -> CURLcode;
     fn curl_easy_perform(handle: *mut CURL) -> CURLcode;
     fn curl_easy_cleanup(handle: *mut CURL);
+    fn curl_easy_strerror(code: CURLcode) -> *const c_char;
+
 }
 
 // 定义一些 libcurl 的常量和数据类型
@@ -18,6 +20,7 @@ const CURLOPT_URL: CURLoption = 10002;
 const CURLOPT_WRITEFUNCTION: CURLoption = 20011;
 const CURLOPT_WRITEDATA: CURLoption = 10001;
 const CURLOPT_FOLLOWLOCATION: CURLoption = 52;
+const CURLOPT_VERBOSE: CURLoption = 41;
 
 extern "C" fn write_callback(
     data: *const c_char,
@@ -26,18 +29,28 @@ extern "C" fn write_callback(
     userdata: *mut std::os::raw::c_void,
 ) -> c_int {
     let buffer = unsafe { &mut *(userdata as *mut Vec<u8>) };
-    let slice = unsafe { slice::from_raw_parts(data as *const u8, (size * nmemb) as usize) };
+    let effective_size = size * nmemb;
+    let slice = unsafe { slice::from_raw_parts(data as *const u8, effective_size as usize) };
     buffer.extend_from_slice(slice);
-    size * nmemb as c_int
+    effective_size as c_int
 }
 
-pub fn curl_get(url: String) -> String {
+// 定义一个函数来获取错误信息
+unsafe fn get_error_message(code: CURLcode) -> String {
+    let error_ptr = curl_easy_strerror(code);
+    CStr::from_ptr(error_ptr).to_string_lossy().into_owned()
+}
+
+pub fn curl_get(url: &str, allow_redirect: bool, verbose: bool) -> Result<String, String> {
+    let mut response_data = Vec::new();
+    let follow_location: c_int = if allow_redirect { 1 } else { 0 };
+    let verbose: c_int = if verbose { 1 } else { 0 };
     unsafe {
+        let url = CString::new(url).map_err(|_| "url should not contains nul byte")?;
         let curl = curl_easy_init();
-        let url = CString::new(url).unwrap();
-        let mut response_data = Vec::new();
         curl_easy_setopt(curl, CURLOPT_URL, url.as_ptr());
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, follow_location);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, verbose);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback as usize);
         curl_easy_setopt(
             curl,
@@ -46,13 +59,13 @@ pub fn curl_get(url: String) -> String {
         );
 
         let result = curl_easy_perform(curl);
-        if result != 0 {
-            return format!("libcurl error: {}", result);
-        }
-
         curl_easy_cleanup(curl);
-        String::from_utf8_lossy(&response_data).to_string()
+        if result != 0 {
+            return Err(get_error_message(result));
+        }
     }
+
+    Ok(String::from_utf8_lossy(&response_data).to_string())
 }
 
 #[cfg(test)]
@@ -61,8 +74,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let s = curl_get("http://httpbin.org/headers".to_string());
-        println!("get {s}");
-        assert!(s.contains("httpbin"));
+        let result = dbg!(curl_get("http://httpbin.org/headers", false).unwrap());
+        assert!(result.contains("httpbin"));
+        let result = dbg!(curl_get("http://baidu.com", true).unwrap());
+        assert!(result.contains("baidu"));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 use curl_sys as sys;
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn curl_get(url: String) -> PyResult<String> {
-    Ok(sys::curl_get(url))
+#[pyo3(signature = (url, redirect = true, verbose = false))]
+fn get(url: &str, redirect: bool, verbose: bool) -> PyResult<String> {
+    let res = sys::curl_get(url, redirect, verbose).map_err(|e| PyValueError::new_err(e))?;
+    Ok(res)
 }
 
 /// Formats the sum of two numbers as string.
@@ -16,6 +19,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn curl_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(curl_get, m)?)?;
+    m.add_function(wrap_pyfunction!(get, m)?)?;
     Ok(())
 }
